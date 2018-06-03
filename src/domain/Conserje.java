8e4c7b12e49a83816eb09c2848f2a049bdf02d08
Chiappe <Franco_Chiappe@hotmail.com>
2018-06-03 20:56:38 -0300

    Prueba con este programa

--- src/domain/Conserje.java
+++ src/domain/Conserje.java
@@ -1,4 +1,21 @@
 package domain;
 
+import java.util.HashMap;
+
 public class Conserje extends Usuario{
+	Pasajero Pasajero;
+	Habitacion habitacion= new Habitacion();
+	HashMap<Integer, Habitacion>ListaHabitaciones;
+
+	public void listar(String nomPelicula)
+	{
+	    Iterator it = MapaPelicula.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry me = (Map.Entry<Integer, PeliculaTO>) it.next();
+	        PeliculaTO pelicula = (PeliculaTO) me.getValue();
+	        if (pelicula.getTitulo() == nomPelicula)
+	        {
+	          MapaPelicula.remove( me.getKey());
+	        }
+	    }
 }
