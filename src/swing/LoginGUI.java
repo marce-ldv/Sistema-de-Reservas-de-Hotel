01e15dcf6aa8d43e8fd02b454616e722671bd177
marce sanchez <marcelo.docutec@gmail.com>
2018-06-03 17:36:10 -0300

    Se agrego un ActionListener para el boton Loguearse y se agregaron comentarios en el JavaDoc

--- src/swing/LoginGUI.java
+++ src/swing/LoginGUI.java
@@ -15,12 +15,19 @@ public class LoginGUI extends JFrame implements ActionListener{
     private JLabel jlabelUsuario;
     private JLabel jlabelPassword;
 
+    /**
+     * Constructor que llama al constructor por defecto de la clase padre JFrame
+     * llamada a los metodos configurar ventana e inicializar componentes
+     */
     public LoginGUI() {
         super();                    // usamos el contructor de la clase padre JFrame
         configurarVentana();        // configuramos la ventana
         inicializarComponentes();   // inicializamos los atributos o componentes
     }
 
+    /**
+     * Este metodo se encarga de configurar la ventana principal
+     */
     private void configurarVentana() {
         this.setTitle("Login");                   // colocamos titulo a la ventana
         this.setSize(800, 600);                                 // colocamos tamanio a la ventana (ancho, alto)
@@ -30,6 +37,10 @@ public class LoginGUI extends JFrame implements ActionListener{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // hacemos que cuando se cierre la ventana termina todo proceso
     }
 
+    /**
+     * Este metodo se encarga de inicializar los componentes
+     * es decir crear paneles, instanciar JLabels, crear botones, etc
+     */
     private void inicializarComponentes() {
         // creamos los componentes
         setIconImage(getIconImage());
@@ -56,6 +67,11 @@ public class LoginGUI extends JFrame implements ActionListener{
         btnLoguearse = new javax.swing.JButton();
         btnLoguearse.setBounds(300,450,170,40);
         btnLoguearse.setText("Iniciar Sesion");
+        btnLoguearse.addActionListener(new java.awt.event.ActionListener(){
+            public void actionPerformed(java.awt.event.ActionEvent evt){
+                btnLoguearseActionPerformed(evt);
+            }
+        });
 
         jlabelUsuario = new javax.swing.JLabel("Usuario: ");
         jlabelUsuario.setBounds(300,300,170,30);
@@ -72,6 +88,10 @@ public class LoginGUI extends JFrame implements ActionListener{
         this.add(jPanel);
     }
 
+    /**
+     * Este metodo se encarga de devolver un objeto de tipo Image
+     * Lo utilizamos para setear un icono a la ventana del jframe actual
+     */
     @Override
     public Image getIconImage() {
         Image miImagen;
@@ -81,16 +101,35 @@ public class LoginGUI extends JFrame implements ActionListener{
         return miImagen; //devuelve una imagen
     }
 
+    /**
+     * Este metodo es un metodo abstracto que esta sobreescrito de la implementacion de la
+     * interfaz ActionListener
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         String nombre = textFieldUsername.getText();                                 // obtenemos el contenido de la caja de texto
         JOptionPane.showMessageDialog(this, "probando actionPErformed");    // mostramos un mensaje (frame, mensaje)
     }
 
+    /**
+     * En este metodo se ejecutara al presionar el boton btnSalir
+     */
     private void btnSalirActionPerformed(java.awt.event.ActionEvent evt){
         dispose();
     }
 
+    /**
+     * En este metodo se ejecutara al presionar el boton btnSalir
+     */
+    private void btnLoguearseActionPerformed(java.awt.event.ActionEvent evt){
+        //aqui se programara el boton iniciar sesion
+    }
+
+    /**
+     * Main method, utiliza la interfaz Runnable para que podamos ejecutar
+     * varios hilos a la vez y el programa no tenga que esperar el fin de la ejecucion de cada hilo
+     * En el metodo run se le da una visibilidad de true
+     */
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable(){
             @Override
