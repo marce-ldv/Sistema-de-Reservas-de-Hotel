90aafe630c1161927f54070010619f75e0de10e3
marce sanchez <marcelo.docutec@gmail.com>
2018-06-05 01:36:52 -0300

    Se modfiico el metodo de validacion del login en la clase Usuario

--- src/domain/Usuario.java
+++ src/domain/Usuario.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 public class Usuario{
 
     private String usuario,password;
+    private Integer idUsuario;
 
     public Usuario(){
         usuario = "Admin";
@@ -22,6 +23,10 @@ public class Usuario{
         return password;
     }
 
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
     /**
      * Este metodo permite hacer la validacion del login
      * Recibe como argumentos las cadenas de texto del usuario y contraseña
@@ -30,7 +35,19 @@ public class Usuario{
 
         if((usuario.equals(user)) && (password.equals(pass))){
             return true;
-        }else {
+        }else if((usuario.equals("")) && (password.equals(""))){
+            JOptionPane.showMessageDialog(null,"Debe completar todos los campos");
+            return false;
+        }else if((usuario.equals(""))){
+            JOptionPane.showMessageDialog(null,"El campo usuario esta vacio");
+            return false;
+        }else if((password.equals(""))){
+            JOptionPane.showMessageDialog(null,"El campo password esta vacio");
+            return false;
+        }else if((usuario.compareTo(user) != 0) && (password.compareTo(pass) != 0)){
+            JOptionPane.showMessageDialog(null,"Usuario o contrasenia incorrectos");
+            return false;
+        }else{
             return false;
         }
     }
