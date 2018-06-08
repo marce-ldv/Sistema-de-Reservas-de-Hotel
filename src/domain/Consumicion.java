649e32818626a894f6ff780c72792400666bee2f
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-06-08 17:25:48 -0300

    Se agrego datos a la clase consumicion y pasajero

--- src/domain/Consumicion.java
+++ src/domain/Consumicion.java
@@ -1,5 +1,82 @@
 package domain;
 
-public class Consumicion {
+public class Consumicion 
+{
+	private double costo;
+	private int idReserva;
+	
+	public Consumicion() 
+	{
+		super();
+		costo = 0;
+		idReserva = 0;
+	}
+	
+	public Consumicion(double costoRecib, int idReservaRecib) 
+	{
+		super();
+		costo = costoRecib;
+		idReserva = idReservaRecib;
+	}
+	
+	public double getCosto()
+	{
+		return costo;
+	}
+	
+	public void setCosto(double nuevoCosto)
+	{
+		costo = nuevoCosto;
+	}
 
+
+	public int getIdReserva() 
+	{
+		return idReserva;
+	}
+
+	public void setIdReserva(int idReserva) 
+	{
+		this.idReserva = idReserva;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "Consumicion [costo=" + costo + ", idReserva=" + idReserva + "]";
+	}
+	
+	public double getCostoConsumicion() 
+	{
+		
+		
+		return 0;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/domain/Pasajero.java
+++ src/domain/Pasajero.java
@@ -1,6 +1,122 @@
 package domain;
 
-public class Pasajero {
+import java.util.List;
 
+public class Pasajero 
+{
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private String numTarjetaCredito;
+	private String telefono;
+	private String Nacionalidad;
+	private List historial;
+	private int idPasajero;
+	
+	public Pasajero() 
+	{
+		super();
+		nombre = null;
+		apellido = null;
+		dni = null;
+		numTarjetaCredito = null;
+		telefono = null;
+		Nacionalidad = null;
+		historial = null;
+		idPasajero = 0;
+	}
+	
+	public Pasajero(String nombreRecib, String apellidoRecib, String dniRecib, String numTarjetaCreditoRecib, String telefonoRecib,
+			String nacionalidadRecib, List historialRecib, int idPasajeroRecib) 
+	{
+		super();
+		nombre = nombreRecib;
+		apellido = apellidoRecib;
+		dni = dniRecib;
+		numTarjetaCredito = numTarjetaCreditoRecib;
+		telefono = telefonoRecib;
+		Nacionalidad = nacionalidadRecib;
+		historial = historialRecib;
+		idPasajero = idPasajeroRecib;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNumTarjetaCredito() {
+		return numTarjetaCredito;
+	}
+
+	public void setNumTarjetaCredito(String numTarjetaCredito) {
+		this.numTarjetaCredito = numTarjetaCredito;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getNacionalidad() {
+		return Nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		Nacionalidad = nacionalidad;
+	}
+
+	public List getHistorial() {
+		return historial;
+	}
+
+	public void setHistorial(List historial) {
+		this.historial = historial;
+	}
+
+	public int getIdPasajero() {
+		return idPasajero;
+	}
+
+	public void setIdPasajero(int idPasajero) {
+		this.idPasajero = idPasajero;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "Pasajero [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", numTarjetaCredito="
+				+ numTarjetaCredito + ", telefono=" + telefono + ", Nacionalidad=" + Nacionalidad + ", historial="
+				+ historial + ", idPasajero=" + idPasajero + "]";
+	}
+	
+	public void llamarConserje() // ..... // implements Conserje ?, quiza ?
+	{
+		
+	}
+	
+	
+	
 }
